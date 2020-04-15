da16786eeb95cb4ddd1f53a15964059b71c6f883
Juan Ezquerro LLanes <arrase@gmail.com>
2020-04-15 13:42:24 +0200

    tv example

--- src/examples/shell_runner/shell_runner.py
+++ src/examples/shell_runner/shell_runner.py
@@ -9,6 +9,30 @@ logger = logging.getLogger(__name__)
 
 logging.getLogger('agt.alexa_gadget').setLevel(logging.DEBUG)
 
+#########################
+#  SCRIPT tv.sh example #
+#########################
+
+#     #!/bin/bash
+#
+#     if [[ "$1" == "on" ]]
+#     then
+#       echo 'on 0.0.0.0' | cec-client -s -d 1
+#       exit 0
+#     fi
+#     
+#     if [[ "$1" == "off" ]]
+#     then
+#       echo 'standby 0.0.0.0' | cec-client -s -d 1
+#       exit 0
+#     fi
+#     
+#     if [[ "$1" == "as" ]]
+#     then
+#       echo 'as' | cec-client -s -d 1
+#       exit 0
+#     fi
+
 class ShellRunnerGadget(AlexaGadget):
     def __init__(self):
         super().__init__()
